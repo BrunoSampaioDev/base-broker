b1360d11ce6f3b91a2073d5916b3c35ff30db5c5
Bruno Sampaio <sampaionascimento100@gmail.com>
2026-04-05 14:22:38 -0300

    feat/add e2e tests (#3)
    * chore: install cypress
    
    * test: add e2e tests
    
    * chore: configure coverage to cypress
    
    * chore: update workflow for run cypress
    
    * chore: update doc
    
    * chore: update db
    
    * chore: stabilize Cypress filter tests to prevent DOM detach flake in CI

---
+++ cypress.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "cypress";
+import codeCoverageTask from "@cypress/code-coverage/task";
+
+export default defineConfig({
+  projectId: "123456",
+  video: true,
+  screenshotOnRunFailure: true,
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
+    pageLoadTimeout: 30000,
+    chromeWebSecurity: false,
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config);
+      return config;
+    },
+  },
+});
